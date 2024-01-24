b69fd28add7258ce4e186f3d92e0150c8c3e584c
Fallflower <xy673636000@gmail.com>
2024-01-24 20:22:23 +0800

    static pie chart

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, Response
 from pyecharts.charts import Bar
 from pyecharts import options as opts
+import json
 import re
 
 app = Flask(__name__)
@@ -46,6 +47,47 @@ def page1():
     chart_options = bar_chart.dump_options()
     # return jsonify(chart_options)
     return render_template('page1.html', chart_options=chart_options)
+
+
+@app.route("/page2")
+def page2():
+    return render_template('page2.html')
+
+
+@app.route("/page3")
+def page3():
+    return render_template('page3.html')
+
+
+@app.route("/page3_data", methods=['GET'])
+def page3_data():
+    return jsonify(male_num=100, female_num=200, data_list=['男', '女'])
+
+# @app.route("/page2_data")
+# def page2_data():
+#     data_list = {}
+#     data1 = ['公众号：Python研究者', '直达', '营销广告', '搜索引擎', '邮件营销', '联盟广告', '视频广告', '百度', '谷歌',
+#              '必应', '其他']
+#     data_list['data1'] = data1
+#     data2 = [
+#         {'value': 2000, 'name': 'Python研究者', 'selected': True},
+#         {'value': 1548, 'name': '搜索引擎'},
+#         {'value': 775, 'name': '直达'},
+#         {'value': 679, 'name': '营销广告'}
+#     ]
+#     data_list['data2'] = data2
+#     data3 = [
+#         {'value': 1048, 'name': '百度'},
+#         {'value': 335, 'name': '直达'},
+#         {'value': 310, 'name': '邮件营销'},
+#         {'value': 251, 'name': '谷歌'},
+#         {'value': 234, 'name': '联盟广告'},
+#         {'value': 147, 'name': '必应'},
+#         {'value': 135, 'name': '视频广告'},
+#         {'value': 102, 'name': '其他'}
+#     ]
+#     data_list['data3'] = data3
+#     return Response(json.dumps(data_list), mimetype='application/json')
 
 
 if __name__ == '__main__':
